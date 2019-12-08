0b26883691a6a173d8040568e5769ef562d40bd3
EShau <EShau00@stuy.edu>
2019-12-08 00:20:07 -0500

    first two deserve a victory applause

--- shell.c
+++ shell.c
@@ -4,13 +4,16 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 
-char ** parse_command( int i, int argc, char * argv[]);
-void run_command( int i, int argc, char * argv[] );
+char ** parse_command( int i, char * argv[]);
+void run_command( int i, char * argv[] );
+void run_commands( int f_i, char * argv[] );
+void handleLine();
+void space_split( char * line, char * argv[] );
 
-char ** parse_command( int i, int argc, char * argv[]){
-  char ** command = malloc(6 * sizeof(char *));
+char ** parse_command( int i, char * argv[] ){
+  char ** command = calloc(6, sizeof(char *));
   int c_i = 0;
-  while (i < argc && strcmp(argv[i], ";")){
+  while (argv[i] && strcmp(argv[i], ";")){
     command[c_i] = argv[i];
     c_i++;
     i++;
@@ -18,32 +21,57 @@ char ** parse_command( int i, int argc, char * argv[]){
   return command;
 }
 
-void run_command( int i, int argc, char * argv[] ){
-  char ** command = parse_command(i, argc, argv);
+void run_command( int i, char * argv[] ){
+  char ** command = parse_command(i, argv);
   execvp(command[0], command);
+}
+
+void run_commands( int f_i, char * argv[] ){
+  for (int i = f_i; argv[i]; i++){
+      int pid = 1;
+      if (i == 1 || !strcmp(argv[i - 1], ";")){
+        pid = fork();
+      }
+      if (pid == 0){
+        run_command(i, argv);
+      }
+      else{
+        wait(NULL);
+      }
+  }
+}
+
+void space_split( char * line, char * argv[] ){
+  char * cur;
+  for (int i = 1; cur; i++) {
+    cur = strsep(&line, " ");
+    argv[i] = cur;
+  }
+}
+
+void handleLine(){
+  char line[100];
+  fgets(line, 100, stdin);
+  line[strlen(line) - 1] = 0;
+  char * refresh = "";
+  printf("%s", refresh);
+  if (!strlen(line)){
+    return;
+  }
+  else{
+    char * argv[51];
+    space_split(line, argv);
+    run_commands(1, argv);
+    handleLine();
+  }
 }
 
 int main(int argc, char * argv[]){
   if (argc > 1){
-    for (int i = 1; i < argc; i++){
-      int pid = fork();
-      if (pid == 0){
-        // printf("I'm a child : %d\n", getpid());
-        // printf("i : %d\n", i);
-        // printf("argv[i - 1] : %s\n", argv[i - 1]);
-        // printf("argv[i] : %s\n", argv[i]);
-        // printf("strcmp : %d\n", strcmp(argv[i - 1], ";"));
-        if (i == 1 || !strcmp(argv[i - 1], ";")){
-          run_command(i, argc, argv);
-        }
-        // printf("That's a yikes\n");
-        exit(0);
-      }
-      else{
-        wait(NULL);
-        printf("I'm the parent who's gonna mess up their lives\n");
-      }
-    }
+    run_commands(1, argv);
+  }
+  else{
+    handleLine();
   }
   // char * cur;
   // cur = argv[1];
