3927a628a482134c7d58f846b22800bc298cac76
EShau <EShau00@stuy.edu>
2019-12-10 16:31:24 -0500

    ok

--- shell.c
+++ shell.c
@@ -6,77 +6,105 @@
 
 char ** parse_command( int i, char * argv[]);
 void run_command( int i, char * argv[] );
-void run_commands( int f_i, char * argv[] );
+void run_commands( char * argv[] );
 void handleLine();
+int is_number( char * string );
 void space_split( char * line, char * argv[] );
 
 char ** parse_command( int i, char * argv[] ){
   char ** command = calloc(6, sizeof(char *));
   int c_i = 0;
-  while (argv[i] && strcmp(argv[i], ";")){
-    command[c_i] = argv[i];
-    c_i++;
-    i++;
+  char * input = argv[i];
+  char * cur = strsep(&input, " ");
+  while (cur){
+    if (strlen(cur)){
+      command[c_i] = cur;
+      c_i++;
+    }
+    cur = strsep(&input, " ");
   }
   return command;
 }
 
 void run_command( int i, char * argv[] ){
   char ** command = parse_command(i, argv);
-  execvp(command[0], command);
+  int j;
+  for (j = 0; command[j]; j++){
+    printf("%s\n", command[j]);
+    if (!strcmp(command[j], "exit")){
+      if (is_number(command[j + 1])){
+        printf("ok\n");
+        kill(getppid(), sscanf("%d", command[j + 1]));
+        exit(sscanf("%d", command[j + 1]));
+      }
+      else{
+        printf("not ok\n");
+        kill(getppid(), SIGKILL);
+        exit(0);
+      }
+    }
+    if (!strcmp(command[j], "cd")){
+      char * directory = "..";
+      if (command[j + 1]){
+        strcpy(directory, command[j + 1]);
+      }
+      chdir(directory);
+    }
+  }
+  int result = execvp(command[0], command);
+  if (result < 0){
+    printf("result: %s\n", strerror(result));
+  }
 }
 
-void run_commands( int f_i, char * argv[] ){
-  for (int i = f_i; argv[i]; i++){
+int is_number( char * string ){
+  return sscanf("%d", string);
+}
+
+void run_commands( char * argv[] ){
+  int i = 0;
+  while (argv[i]){
       int pid = 1;
-      if (i == 1 || !strcmp(argv[i - 1], ";")){
-        pid = fork();
-      }
+      pid = fork();
       if (pid == 0){
         run_command(i, argv);
       }
       else{
-        wait(NULL);
+        int status;
+        wait(&status);
       }
+      i++;
   }
 }
 
-void space_split( char * line, char * argv[] ){
-  char * cur;
-  for (int i = 1; cur; i++) {
-    cur = strsep(&line, " ");
+void find_commands( char * line, char * argv[] ){
+  char * cur = strsep(&line, ";");
+  int i = 0;
+  while (cur) {
     argv[i] = cur;
+    cur = strsep(&line, ";");
+    i++;
+  }
+  if (i > 0){
+    argv[i + 1] = '\0';
   }
 }
 
 void handleLine(){
-  char line[100];
-  fgets(line, 100, stdin);
-  line[strlen(line) - 1] = 0;
-  char * refresh = "";
-  printf("%s", refresh);
-  if (!strlen(line)){
-    return;
+  char line[256];
+  int i;
+  for (i = 0; i < 256; i++){
+    line[i] = '\0';
   }
-  else{
-    char * argv[51];
-    space_split(line, argv);
-    run_commands(1, argv);
-    handleLine();
-  }
+  fgets(line, 256, stdin);
+  line[strlen(line) - 1] = '\0';
+  char * argv[50];
+  find_commands(line, argv);
+  run_commands(argv);
+  handleLine();
 }
 
-int main(int argc, char * argv[]){
-  if (argc > 1){
-    run_commands(1, argv);
-  }
-  else{
-    handleLine();
-  }
-  // char * cur;
-  // cur = argv[1];
-  // char ** args = parse_args(cur);
-  // printf("Running execvp for the invocation...\n");
-  // execvp(args[0], args);
+int main(){
+  handleLine();
   return 0;
 }
