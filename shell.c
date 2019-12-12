f94449bd594c64abfac5873f370a963ee7f60bd0
Vincent Fong <vfong00@stuy.edu>
2019-12-11 20:59:53 -0500

    idk lol

--- shell.c
+++ shell.c
@@ -4,16 +4,15 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 
-char ** parse_command( int i, char * args[]);
-void run_command( int i, char * args[] );
-void run_commands( char * args[] );
+char ** parse_command(char * input);
+void run_command(char * input);
+// void run_commands( char * args[] );
 void handle_line();
 void find_commands( char * line, char * args[] );
 
-char ** parse_command( int i, char * args[] ){
+char ** parse_command(char * input){
   char ** command = calloc(6, sizeof(char *));
   int c_i = 0;
-  char * input = args[i];
   char * cur = strsep(&input, " ");
   while (cur){
     if (strlen(cur)){
@@ -25,15 +24,28 @@ char ** parse_command( int i, char * args[] ){
   return command;
 }
 
-void run_command( int i, char * args[] ){
-  char ** command = parse_command(i, args);
+int cmd_chr(char ** cmd, char find) {
+        char **temp = cmd;  // temp is a pointer to a *pointer*, not a pointer to a *char*
+        while (*temp != NULL) {
+                if (strchr(*temp, find)) return 1;
+                temp++;
+        }
+        return 0;
+}
+
+void redir_in(char * input_prompt) {
+
+}
+
+void run_command(char * input_prompt){
+  char ** command = parse_command(input_prompt);
   int x = 0;
   while (command[x]){
     x++;
   }
   if (!strcmp(command[0], "exit")){
     int num = 0;
-    if (command[1]){
+    if (command[1]) {
       sscanf(command[1], "%d", &num);
       printf("%d\n", num);
     }
@@ -46,9 +58,12 @@ void run_command( int i, char * args[] ){
     }
     chdir(directory);
   }
+  else if (strchr(input_prompt, '<')) {
+          redir_in(input_prompt);
+          return;
+  }
   else{
-    int pid = fork();
-    if (pid == 0){
+    if (!fork()){
       int result = execvp(command[0], command);
       if (result < 0){
         printf("result: %s\n", strerror(result));
@@ -60,13 +75,13 @@ void run_command( int i, char * args[] ){
   }
 }
 
-void run_commands( char * args[] ){
-  int i = 0;
-  while (args[i]){
-    run_command(i, args);
-    i++;
-  }
-}
+// void run_commands( char * args[] ){
+//   int i = 0;
+//   while (args[i]){
+//     run_command(i, args);
+//     i++;
+//   }
+// }
 
 void find_commands( char * line, char * args[] ){
   char * cur = strsep(&line, ";");
@@ -77,6 +92,13 @@ void find_commands( char * line, char * args[] ){
     i++;
   }
   args[i] = '\0';
+
+  i = 0;
+  while (args[i]) {
+    run_command(args[i]);
+    i++;
+  }
+
 }
 
 void handle_line(){
@@ -89,11 +111,21 @@ void handle_line(){
   line[strlen(line) - 1] = '\0';
   char * args[50];
   find_commands(line, args);
-  run_commands(args);
-  handle_line();
+  // run_commands(args);
+}
+
+void print_host() {
+        if (isatty(STDIN_FILENO)) {
+                char * cwd = getcwd(NULL, 1024);
+                printf("\e[1;104m%s %s$\e[0m ", cwd, getlogin());
+                free(cwd);
+        }
 }
 
 int main(){
-  handle_line();
+        while(1) {
+                print_host();
+                handle_line();
+        }
   return 0;
 }
