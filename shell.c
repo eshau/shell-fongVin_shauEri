5330b38c99cab4e5833d5e8e775c29cebbd253cf
Vincent Fong <vfong00@stuy.edu>
2019-12-16 05:28:35 -0500

    done done

--- shell.c
+++ shell.c
@@ -20,7 +20,7 @@ void handle_line() {
         fgets(line, 256, stdin);
         line[strlen(line) - 1] = '\0';
         char * args[50];
-        
+
         process_commands(line, args);
 }
 
@@ -28,12 +28,17 @@ void handle_line() {
 void print_host() {
         if (isatty(STDIN_FILENO)) {
                 char * cwd = getcwd(NULL, 1024);
-                printf("\e[91m> \e[0;1;92m%s\e[0;1;91m: \e[0;96m%s\e[0;91m$\e[0m ", cwd, getlogin());
+                printf("\e[0;91m> \e[0;1;92m%s\e[0;1;91m: \e[0;96m%s\e[0;91m$\e[0m ", cwd, getlogin());
                 free(cwd);
         }
 }
 
-int main(){
+int main() {
+        printf("\e[92m-----------------------------------------------\n");
+        printf("\e[92mWelcome to \e[0;91mEric\e[0;92m and \e[0;96mVincent's \e[0;92mshell.\n");
+        printf("Try not to break \e[0;1;4;91mtoo\e[0;92m much.\n\n");
+        printf("\e[0;1;4;95mQUOTE OF THE DAY:\e[0;92m \"I know not all that may be coming, but be it what it will, I'll go to it laughing.\" -- \e[0;3;92mHerman Melville\e[0;92m, \e[4mMoby Dick\n");
+        printf("\e[0;92m-----------------------------------------------\n");
         while(1) {
                 print_host();
                 handle_line();
