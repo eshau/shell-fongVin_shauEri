9e05cd51daa10f29c152b5973005b3b1fd7f4ba6
Vincent Fong <vfong00@stuy.edu>
2019-12-16 03:45:31 -0500

    more stuff

--- commands.h
+++ commands.h
@@ -6,8 +6,8 @@
 #include <fcntl.h>
 #include <errno.h>
 
-void process_commands(char * line, char * args[]);
-void run_command(char * input);
 int redir_in(char * input);
 int redir_out(char * input);
 int pipes(char * input);
+void run_command(char * input);
+void process_commands(char * line, char * args[]);
