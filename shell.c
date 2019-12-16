9323a20d810f10e326644a5ff7af210826a92e6d
Vincent Fong <vfong00@stuy.edu>
2019-12-15 21:42:16 -0500

    wow first commit in 3 days i kind of forgot to do this for a while

--- shell.c
+++ shell.c
@@ -3,117 +3,173 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+#include <fcntl.h>
+#include <errno.h>
 
-char ** parse_command(char * input, char * sep);
-void run_command(char * input);
-// void run_commands( char * args[] );
 void handle_line();
-void find_commands( char * line, char * args[] );
+void process_commands(char * line, char * args[]);
+void run_command(char * input);
+int redir_in(char * input);
+int redir_out(char * input);
+char ** parse_command(char * input, char * sep);
+char * strip(char * line);
 
 char ** parse_command(char * input, char * sep) {
-  char ** command = calloc(6, sizeof(char *));
-  int c_i = 0;
-  char * cur = strsep(&input, sep);
-  while (cur){
-    if (strlen(cur)){
-      command[c_i] = cur;
-      c_i++;
-    }
-    cur = strsep(&input, sep);
-  }
-  return command;
-}
-
-int cmd_chr(char ** cmd, char find) {
-        char **temp = cmd;  // temp is a pointer to a *pointer*, not a pointer to a *char*
-        while (*temp != NULL) {
-                if (strchr(*temp, find)) return 1;
-                temp++;
+        char ** command = calloc(6, sizeof(char *));
+        int c_i = 0;
+        char * cur = strsep(&input, sep);
+        while (cur) {
+            if (strlen(cur)){
+                    command[c_i] = cur;
+                    c_i++;
+            }
+            cur = strsep(&input, sep);
         }
-        return 0;
+        return command;
 }
 
-void redir_in(char * input_prompt) {
+char * strip(char * line) {
+        char* start = line;
+        while (*start == ' ') {
+                start++;
+        }
+        char* end = line;
+        while (*end) {
+                end++;
+        }
+        end--;
+        while (*end == ' ') {
+                *end = 0;
+                end--;
+        }
+        return start;
+}
+
+int redir_in(char * input) {
+        if (strchr(input, '<')) {
+                char ** args = parse_command(input, "<");
+                if (!fork()) {
+                        int i = 1;
+                        int fd;
+                        char* filename;
+                        while (args[i]) {
+                                fd = open(strip(args[i]), O_RDONLY);
+                                if (fd == -1) printf("%s\n", strerror(errno));
+                                i++;
+                        }
+                        dup2(fd, STDIN_FILENO);
+                        char ** stuff = parse_command(args[0], " ");
+
+                        int error = execvp(stuff[0], stuff);
+                        if (error == -1) {
+                                printf("%s : Nice going, you messed up somewhere!\n", args[0]);
+                                printf("%s\n", strerror(errno));
+                        }
+                }
+                return 1;
+        }
+        else return 0;
+}
+
+int redir_out(char * input) {
+        if (strchr(input, '>')) {
+                char ** args = parse_command(input, ">");
+                if (!fork()) {
+                        int i = 1;
+                        int fd;
+                        char* filename;
+                        while (args[i]) {
+                                fd = open(strip(args[i]), O_WRONLY | O_CREAT, 0644);
+                                if (fd == -1) printf("%s\n", strerror(errno));
+                                i++;
+                        }
+                        dup2(fd, STDOUT_FILENO);
+                        char ** commands = parse_command(args[0], " ");
+
+                        int error = execvp(commands[0], commands);
+                        if (error == -1) {
+                                printf("%s : TNice going, you messed up somewhere!\n", args[0]);
+                                printf("%s\n", strerror(errno));
+                        }
+                }
+                return 1;
+        }
+        else return 0;
+}
+
+void run_command(char * input) {
+        // TBH I don't know why but after parse_command is done, new copies of the input are needed. Ask Eric lol
+        char * r_in = calloc(sizeof(char), 1024);
+        strcpy(r_in, input);
+        char * r_out = calloc(sizeof(char), 1024);
+        strcpy(r_out, input);
+
+        char ** command = parse_command(input, " ");
+        int x = 0;
+        if (!strcmp(command[0], "exit")){
+                int num = 0;
+                if (command[1]) {
+                        sscanf(command[1], "%d", &num);
+                }
+                exit(num);
+        }
+        else if (!strcmp(command[0], "cd")){
+                char directory[100] = "..";
+                if (command[1]){
+                        strcpy(directory, command[1]);
+                }
+                chdir(directory);
+        }
+        else if (redir_in(r_in)) return;
+        else if (redir_out(r_out)) return;
+        else {
+                if (!fork()){
+                        int result = execvp(command[0], command);
+                        if (result < 0){
+                                printf("result: %s\n", strerror(result));
+                        }
+                }
+                else {
+                        wait(NULL);
+                }
+                return;
+        }
+}
+
+void process_commands( char * line, char * args[] ) {
+        // finding commands
+        char * cur = strsep(&line, ";");
+        int i = 0;
+        while (cur) {
+                args[i] = cur;
+                cur = strsep(&line, ";");
+                i++;
+        }
+        args[i] = '\0';
+
+        // running commands
+        i = 0;
+        while (args[i]) {
+                run_command(args[i]);
+                i++;
+        }
 
 }
 
-void run_command(char * input_prompt){
-  char ** command = parse_command(input_prompt, " ");
-  int x = 0;
-  while (command[x]){
-    x++;
-  }
-  if (!strcmp(command[0], "exit")){
-    int num = 0;
-    if (command[1]) {
-      sscanf(command[1], "%d", &num);
-      printf("%d\n", num);
-    }
-    exit(num);
-  }
-  else if (!strcmp(command[0], "cd")){
-    char directory[100] = "..";
-    if (command[1]){
-      strcpy(directory, command[1]);
-    }
-    chdir(directory);
-  }
-  else if (strchr(input_prompt, '<')) {
-          redir_in(input_prompt);
-          return;
-  }
-  else{
-    if (!fork()){
-      int result = execvp(command[0], command);
-      if (result < 0){
-        printf("result: %s\n", strerror(result));
-      }
-    }
-    else{
-      wait(NULL);
-    }
-  }
+void handle_line() {
+        char line[256];
+        int i;
+        for (i = 0; i < 256; i++){
+                line[i] = '\0';
+        }
+        fgets(line, 256, stdin);
+        line[strlen(line) - 1] = '\0';
+        char * args[50];
+
+        process_commands(line, args);
 }
 
-// void run_commands( char * args[] ){
-//   int i = 0;
-//   while (args[i]){
-//     run_command(i, args);
-//     i++;
-//   }
-// }
-
-void find_commands( char * line, char * args[] ){
-  char * cur = strsep(&line, ";");
-  int i = 0;
-  while (cur) {
-    args[i] = cur;
-    cur = strsep(&line, ";");
-    i++;
-  }
-  args[i] = '\0';
-
-  i = 0;
-  while (args[i]) {
-    run_command(args[i]);
-    i++;
-  }
-
-}
-
-void handle_line(){
-  char line[256];
-  int i;
-  for (i = 0; i < 256; i++){
-    line[i] = '\0';
-  }
-  fgets(line, 256, stdin);
-  line[strlen(line) - 1] = '\0';
-  char * args[50];
-  find_commands(line, args);
-  // run_commands(args);
-}
-
+// prints a pretty prompt thing I guess.
 void print_host() {
         if (isatty(STDIN_FILENO)) {
                 char * cwd = getcwd(NULL, 1024);
